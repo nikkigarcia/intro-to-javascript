9dc56b9cdcc0f761037f85e4641ee86a8b89857c
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-04-25 16:08:46 -0700

    car make and model concatination

--- js/review-string-methods.js
+++ js/review-string-methods.js
@@ -2,3 +2,9 @@ var str1 = 'Hello World!';
 console.log(str1);
 console.log('Hello');
 console.log(str1.slice(6));
+
+var carMake = 'Mistubishi';
+var carModel = 'Galant';
+console.log(carMake, carModel);
+var car = carMake + ' ' + carModel;
+console.log(car);
