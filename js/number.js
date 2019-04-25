b2660484f4fe048a850e89dc3b8892b9d126432f
Nikki Garcia <yurfavorite_girl@yhaoo.com>
2019-04-25 14:24:48 -0700

    number variable calculations

---
+++ js/number.js
@@ -0,0 +1,12 @@
+var a = 88;
+var b = 7;
+var sum = a + b;
+console.log(sum)
+var diff = a - b;
+console.log(diff);
+var mult = a * b;
+console.log(mult);
+var divi= a/b;
+console.log(divi);
+var mod = a % b;
+console.log(mod);
